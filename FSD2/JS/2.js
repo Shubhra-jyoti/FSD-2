3a65db8efa8b4a73296478db48fc7e3c0742997c
Shubhra-jyoti <sj.brahma2006@gmail.com>
2026-03-09 13:26:14 +0530

    Add files via upload

---
+++ FSD2/JS/2.js
@@ -0,0 +1,8 @@
+//
+date=JSON.parse('{"date":"2026-03-09"}')
+a=new Date(date.date)
+console.log(a)
+
+function showdate(){
+    document.writeln(a)
+}
---
+++ FSD2/JS/script.js
@@ -0,0 +1,19 @@
+    a={"name":"abc","age":28,"address":{"city":"Ahmedabad","state":"Gujarat"},"subjects":["fsd","python","Toc"],"rank":null};
+    console.log(a)
+    const obj=require("../JSON/p.json")
+    console.log(obj)
+    
+    var a={"book":[
+        {"name":"abc","age":28,"address":{"city":"Ahmedabad","state":"Gujarat"},"subjects":["fsd","python","Toc"],"rank":null},
+        {"name":"java","age":28,"address":{"city":"Ahmedabad","state":"Gujarat"},"subjects":["fsd","python","Toc"],"rank":null,"version":"3rd","author":"J.Shephard"}
+
+
+    ]}
+    console.log(`we have ${a.book[1].name} but version is ${a.book[1].version}  and author is ${a.book[1].author}`)
+    let p={"name":"hjdhd",age:32}
+   let key="age"
+   p[key]=45
+   console.log(p)
+
+
+    
